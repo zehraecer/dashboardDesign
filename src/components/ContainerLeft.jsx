d30599920290ea54de8cd16e5586b0244c1a7935
Zehra Ecer <zehraecer1996@hotmail.com>
2024-08-23 21:49:54 +0300

    cursor pointer eklendi

--- src/components/ContainerLeft.jsx
+++ src/components/ContainerLeft.jsx
@@ -6,19 +6,12 @@ export const ContainerLeft = ({ onLogoClick, mood, isClicked }) => {
 
         <div className="d-none d-flex flex-column justify-content-between align-content-between container-left gap-1 d-lg-flex left-heigth middle-mode h-100">
             <div >
-                <div>
-                    <img
-                        style={{ width: "26px", height: "30px", cursor: "pointer" }}
-                        src="logoIcon.svg"
-                        alt=""
-                        onClick={onLogoClick}
-                    />
-                </div>
-                <div><img onClick={onLogoClick} src="search.svg" alt="" /></div>
-                <div><img onClick={onLogoClick} src="horizontal.svg" alt="" /></div>
-                <div> <img onClick={onLogoClick} src="calendar.svg" alt="" /></div>
-                <div><img onClick={onLogoClick} src="alt.svg" alt="" /></div>
-                <div><img onClick={mood} src={isClicked ? "moon.svg" : "sun.svg"} alt="" /></div>
+                <div><img style={{ width: "26px", height: "30px", cursor: "pointer" }} src="logoIcon.svg" onClick={onLogoClick} /></div>
+                <div><img role="button" onClick={onLogoClick} src="search.svg" alt="" /></div>
+                <div><img role="button" onClick={onLogoClick} src="horizontal.svg" alt="" /></div>
+                <div> <img role="button" onClick={onLogoClick} src="calendar.svg" alt="" /></div>
+                <div><img role="button" onClick={onLogoClick} src="alt.svg" alt="" /></div>
+                <div><img role="button" onClick={mood} src={isClicked ? "moon.svg" : "sun.svg"} alt="" /></div>
             </div>
             <div className="flex-grow-1"></div>
             <div className="d-flex flex-column ">
