115946a1d5c2ddf36c838998f5f05eabcba588c3
Zehra Ecer <zehraecer1996@hotmail.com>
2024-08-24 20:50:34 +0300

    mobil navbar düzenlendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,14 @@ export const Header = ({ mood, isClicked }) => {
         <>
 
             <div className="d-block d-md-none">
-                <nav className="navbar bg-body-tertiary fixed-top">
+                <nav className="navbar bg-body-tertiary fixed-top mobile-mode">
                     <div className="container-fluid">
-                        <img src="Lookscout.svg" alt="" />
+                        <img src={isClicked ? "lookscout-white.svg" : "Lookscout.svg"} />
 
-                        <div className="d-flex justify-content-center align-items-center gap-3">
+                        <div className="d-flex justify-content-center align-items-center gap-3 ">
                             <div>
                                 <div className="d-flex justify-content-center align-items-center gap-3">
-                                    <img onClick={mood} src={isClicked ? "moon.svg" : "sun.svg"} alt="" />
+                                    <img onClick={mood} src={isClicked ? "sun2.svg" : "moon.svg"} alt="" />
                                     <button
                                         className="navbar-toggler"
                                         type="button"
@@ -22,42 +22,68 @@ export const Header = ({ mood, isClicked }) => {
                                         data-bs-target="#offcanvasNavbar"
                                         aria-controls="offcanvasNavbar"
                                         aria-label="Toggle navigation">
-                                        <img src="hamburgerMenu.svg" alt="" />
+                                        <img src={isClicked ? "hamburger-White.svg" : "hamburgerMenu.svg"} alt="" />
                                     </button>
                                 </div>
                             </div>
                         </div>
                         <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                            <div className="offcanvas-header">
-                                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            <div className="offcanvas-header mobile-mode">
+                                <h5 className="offcanvas-title" id="offcanvasNavbarLabel"> <img src={isClicked ? "lookscout-white.svg" : "Lookscout.svg"} /></h5>
+                                <button type="button" className="btn-close zeze" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
-                            <div className="offcanvas-body">
-                                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li className="nav-item dropdown">
-                                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Dropdown
-                                        </a>
-                                        <ul className="dropdown-menu">
-                                            <li><a className="dropdown-item" href="#">Action</a></li>
-                                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                                            <li>
-                                                <hr className="dropdown-divider" />
-                                            </li>
-                                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <form className="d-flex mt-3" role="search">
-                                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                    <button className="btn btn-outline-success" type="submit">Search</button>
-                                </form>
+                            <div className="offcanvas-body middle-mode ">
+                                <div className="d-flex flex-column align-items-start " >
+                                    <div className="d-flex justify-content-center  gap-0-75rem paddingb-0-75rem">
+                                        <img src="arrowleft.svg" alt="" />
+                                        <span className="container-middle-one" >Lookscout Dashboard</span>
+                                    </div>
+                                    <div className="d-flex form-one gap-.7 border border-2 rounded-2">
+                                        <div><img style={{ width: "16.5px", height: "16.5px" }} src="search.svg" alt="" /></div>
+                                        <div><input className="form-one-1" style={{ width: "206px", height: "20px" }} type="text" placeholder="Search here..." /></div>
+                                    </div>
+
+                                    <div className="d-flex  gap-0-75rem justify-content-center paddingb-0-75rem " style={{ marginTop: "32px" }}>
+                                        <div><img style={{ width: "21px", height: "20px" }} src="horizontal.svg" /></div>
+                                        <div><span className="general">General</span> </div>
+                                    </div>
+                                    <div className="d-flex justify-content-between  paddingb-0-75rem w-100 " >
+                                        <div className="d-flex  gap-0-75rem">
+                                            <div><img style={{ width: "21px", height: "20px" }} src="horizontal.svg" /></div>
+                                            <div >
+                                                <span className="general">Messages </span>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <span className="rounded-5" style={{ padding: "5px 11px", backgroundColor: "#EFEFF1" }}>6</span>
+                                        </div>
+                                    </div>
+                                    <div className="d-flex  gap-0-75rem justify-content-center paddingb-0-75rem ">
+                                        <div><img style={{ width: "21px", height: "20px" }} src="horizontal.svg" /></div>
+                                        <div><span className="general">Notifications</span> </div>
+                                    </div>
+
+                                    <div className="d-flex  gap-0-75rem justify-content-center paddingb-0-75rem ">
+                                        <div><img style={{ width: "21px", height: "20px" }} src="horizontal.svg" /></div>
+                                        <div><span className="general">Users</span> </div>
+                                    </div>
+
+                                    <div className="d-flex  gap-0-75rem justify-content-center paddingb-0-75rem ">
+                                        <div><img style={{ width: "21px", height: "20px" }} src="horizontal.svg" /></div>
+                                        <div><span className="general">Events & Logs</span> </div>
+                                    </div>
+
+                                    <div className="d-flex  gap-0-75rem justify-content-center paddingb-0-75rem ">
+                                        <div><img style={{ width: "21px", height: "20px" }} src="horizontal.svg" /></div>
+                                        <div><span className="general">Organization</span> </div>
+                                    </div>
+
+                                    <div className="d-flex  gap-0-75rem justify-content-center paddingb-0-75rem ">
+                                        <div><img style={{ width: "21px", height: "20px" }} src="horizontal.svg" /></div>
+                                        <div><span className="general">Teams</span> </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
