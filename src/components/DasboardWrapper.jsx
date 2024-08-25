5b1fcd3cd541bb599f07ca8d42c7868f2fca6d50
Zehra Ecer <zehraecer1996@hotmail.com>
2024-08-25 11:35:32 +0300

    tarayıcı hatası çözüldü 2

--- src/components/DasboardWrapper.jsx
+++ src/components/DasboardWrapper.jsx
@@ -14,13 +14,11 @@ export const DashboardWrapper = () => {
     }
 
     useEffect(() => {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 
-            if (isClicked) {
-                document.body.classList.add('dark-mode');
-            } else {
-                document.body.classList.remove('dark-mode');
-            }
+        if (isClicked) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
         }
     }, [isClicked]);
 
